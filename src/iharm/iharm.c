28610fcea990f4f60c6d2613119b6aa62e4aee22
Ben Prather <bprathr2@illinois.edu>
2019-01-07 12:26:22 -0600

    Add electrons flags to iharm output when including them

--- src/iharm/iharm.c
+++ src/iharm/iharm.c
@@ -19,7 +19,16 @@ int iharm_write_header_koral(char *fname, geom_koral *geom) {
   h5io_add_data_int(fid, "/header/n1", geom->nx);
   h5io_add_data_int(fid, "/header/n2", geom->ny);
   h5io_add_data_int(fid, "/header/n3", geom->nz);
-  h5io_add_data_int(fid, "/header/n_prim", 8);
+  int nprim, has_elec;
+  if (geom->dtype == KORAL_RADGRMHD) {
+    nprim = 10;
+    has_elec = 1;
+  } else {
+    nprim = 8;
+    has_elec = 0;
+  }
+  h5io_add_data_int(fid, "/header/n_prim", nprim);
+  h5io_add_data_int(fid, "/header/has_electrons", has_elec);
 
   h5io_add_data_dbl(fid, "/header/geom/startx1", geom->startx[1]);
   h5io_add_data_dbl(fid, "/header/geom/startx2", geom->startx[2]);
