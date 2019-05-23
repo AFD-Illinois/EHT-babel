f602246d11e1ffc4177a682867a9d8f462d275be
Ben Prather <bprathr2@illinois.edu>
2019-05-23 13:46:15 -0500

    Improve documentation

--- python/bhac-translate.py
+++ python/bhac-translate.py
@@ -3,7 +3,7 @@
 ## Translator of BHAC (binary) output to HARM format
 # Currently only supports LOG_KS metric for BHAC output, but is extensible
 
-# Usage bhac_trans.py [--gridfile] file1.blk [file2.blk ...]
+# Usage: bhac-translate.py [--gridfile] file1.blk [file2.blk ...]
 
 import sys
 import numpy as np
@@ -47,10 +47,10 @@ for bfname, harmfname in zip(bhacfnames, harmfnames):
     dim = np.frombuffer(bf.read(4), dtype=np.int32)[0]
     # prec is a (logical) flag to the ray-tracing code for the numerical precision of the GRMHD data (not the header). prec = .TRUE. implies single precision data, whereas prec = .FALSE. implies double precision data.
     prec = np.frombuffer(bf.read(4), dtype=np.int32)[0]
-    
+
     # Because apparently headers need padding idk
     bf.seek(256, 0)
-    
+
     print("Time: {} File size: {}x{}x{}, Spin: {}, Grid start: ({},{},{}) stop: ({},{},{})".format(
            t, N1, N2, N3, a, startx1, startx2, startx3, stopx1, stopx2, stopx3))
 
@@ -75,7 +75,7 @@ for bfname, harmfname in zip(bhacfnames, harmfnames):
     # We'll output in the HARM order/values
     prim_names = [ b"RHO", b"UU", b"U1", b"U2", b"U3", b"B1", b"B2", b"B3" ]
     hdr.create_dataset("prim_names", data=np.array(prim_names, dtype='S'))
-    
+
     # Non-coordinate-specific geometry
     geom = hf.create_group('header/geom')
     geom['startx1'] = startx1
@@ -86,7 +86,7 @@ for bfname, harmfname in zip(bhacfnames, harmfnames):
     #geom['dx2'] = (stopx2 - startx2)/N2
     geom['dx3'] = (stopx3 - startx3)/N3
     geom['n_dim'] = dim + 1 # HARM includes _all_ dimensions
-    
+
     if metric == "LOG_KS":
       # Hack log_ks for Gammie MKS tooling
       hdr['metric'] = "MKS"
@@ -97,12 +97,12 @@ for bfname, harmfname in zip(bhacfnames, harmfnames):
       else:
         bmks['a'] = a
       bmks['hslope'] = 1.
-      bmks['r_in'] = np.exp(startx1)      
+      bmks['r_in'] = np.exp(startx1)
       bmks['r_out'] = np.exp(stopx1)
 
       # Finally, patch theta, which HARM takes to be 0-1
       geom['dx2'] = (stopx2 - startx2)/N2/np.pi
-      
+
     elif metric == "BHAC_MKS":
       print("BHAC-specific MKS not supported!")
       exit(-1)
@@ -114,7 +114,7 @@ for bfname, harmfname in zip(bhacfnames, harmfnames):
     else:
       print("Metric {} not supported!".format(metric))
       exit(-1)
-    
+
     # Convert primitives
     prims = np.zeros_like(prims_bhac)
     # RHO is the same
@@ -129,7 +129,7 @@ for bfname, harmfname in zip(bhacfnames, harmfnames):
     prims[:,:,:,5] = prims_bhac[:,:,:,5]
     prims[:,:,:,6] = prims_bhac[:,:,:,6]
     prims[:,:,:,7] = prims_bhac[:,:,:,7]
-    
+
     hf['prims'] = prims
 
 if write_grid:
@@ -146,39 +146,39 @@ if write_grid:
   X1 = grid[:,:,:,0]
   X2 = grid[:,:,:,1]
   X3 = grid[:,:,:,2]
-  
+
   if metric == "LOG_KS":
     r = np.exp(X1)
     th = X2 # LOG_KS uses X2=th i.e. 0->PI
     phi = X3
-    
+
     # Get the metric
     gcov = np.zeros((N1,N2,N3,4,4))
     gcov[:,:,:,:,:] = gcov_logks_2d(grid[:,0,0,0], grid[0,:,0,1], a)[:,:,None,:,:] #.reshape((N1,N2,1,4,4)).repeat(N3, axis=2)
     gcon = np.linalg.inv(gcov)
     gdet = np.sqrt(-np.linalg.det(gcov))
     lapse = 1/np.sqrt(-gcon[:,:,:,0,0])
-    
+
   elif metric == "BHAC_MKS":
     r = np.exp(X1)
     # TODO th = their thing
   else:
     print("Metric {} not supported!".format(metric))
     exit(-1)
-  
+
   with h5py.File(harmgname,"w") as hf:
     hf['X1'] = X1
     hf['X2'] = X2
     hf['X3'] = X3
-    
+
     hf['r'] = r
     hf['th'] = th
     hf['phi'] = phi
-    
+
     hf['X'] = r*np.sin(th)*np.cos(phi)
     hf['Y'] = r*np.sin(th)*np.sin(phi)
     hf['Z'] = r*np.cos(th)
-    
+
     hf['gcon'] = gcon
     hf['gcov'] = gcov
     hf['gdet'] = gdet
